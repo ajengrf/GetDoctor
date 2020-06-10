5301f8c39ec645563006832c5f941df7b3b33235
ajengrf <ajengrf.12@gmail.com>
2020-06-11 00:57:26 +0700

    Hospitals UI

---
+++ src/components/molecules/ListHospitals/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { DummyHospital1 } from '../../../assets'
+import { colors, fonts } from '../../../utils'
+
+export default function ListHospitals() {
+  return (
+    <View style={styles.container}>
+      <Image source={DummyHospital1} style={styles.picture} />
+      <View>
+        <Text style={styles.title}>Rumah Sakit</Text>
+        <Text style={styles.title}>Citra Bunga Merdeka</Text>
+        <Text style={styles.address}>Jl. Surya Sejahtera 20</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.border
+  },
+  picture: {
+    width: 80,
+    height: 60,
+    borderRadius: 11,
+    marginRight: 16
+  },
+  title: {
+    fontFamily: fonts.primary[600],
+    fontSize: 16,
+    color: colors.text.primary
+  },
+  address: {
+    fontSize: 12,
+    fontFamily: fonts.primary[300],
+    color: colors.text.secondary,
+    marginTop: 6
+  }
+})
--- src/pages/Hospitals/index.js
+++ src/pages/Hospitals/index.js
@@ -1,12 +1,52 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ImageBackground } from 'react-native'
+import { ILHospitalBG } from '../../assets'
+import { fonts, colors } from '../../utils'
+import { ListHospitals } from '../../components'
 
 export default function Hospitals() {
   return (
-    <View>
-      <Text>Hospitals</Text>
+    <View style={styles.page}>
+      <ImageBackground source={ILHospitalBG} style={styles.background} >
+        <Text style={styles.title}>Nearby Hospitals</Text>
+        <Text style={styles.desc}>3 tersedia</Text>
+      </ImageBackground>
+      <View style={styles.content}>
+        <ListHospitals />
+        <ListHospitals />
+        <ListHospitals />
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: colors.secondary,
+    flex: 1
+  },
+  background: {
+    height: 240,
+    paddingTop: 30
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 20,
+    fontFamily: fonts.primary[600],
+    color: colors.white
+  },
+  desc: {
+    textAlign: "center",
+    fontSize: 14,
+    fontFamily: fonts.primary[300],
+    color: colors.white,
+    marginTop: 6
+  },
+  content: {
+    backgroundColor: colors.white,
+    flex: 1,
+    borderRadius: 20,
+    marginTop: -30,
+    paddingTop: 14
+  },
+})

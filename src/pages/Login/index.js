fe001bb143aac83535ca8dc49e5bf724cbabd277
ajengrf <ajengrf.12@gmail.com>
2020-06-13 21:31:04 +0700

    add state management with redux for showing loading and refactoring showMessage for error and success

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,85 +1,82 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import { ILLogo } from '../../assets'
-import { Input, Link, Button, Gap, Loading } from '../../components'
-import { colors, fonts, useForm, storeData } from '../../utils'
+import { Input, Link, Button, Gap } from '../../components'
+import { colors, fonts, useForm, storeData, showError } from '../../utils'
 import { Firebase } from '../../config'
-import { showMessage } from "react-native-flash-message"
+import { useDispatch } from 'react-redux'
 
 export default function Login({ navigation }) {
-  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch()
+
   const [form, setForm] = useForm({
     email: "",
     password: ""
   })
 
+  const loading = (value) => {
+    dispatch({
+      type: "SET_LOADING",
+      value
+    })
+  }
+
   const login = () => {
-    console.log({ form })
-    setLoading(true)
+    loading(true)
+
     Firebase.auth()
       .signInWithEmailAndPassword(form.email, form.password)
       .then(res => {
-        setLoading(false)
-        console.log({ res })
+        loading(false)
 
         Firebase.database()
           .ref(`users/${res.user.uid}/`)
           .once("value")
           .then(user => {
-            console.log({ user: user.val() })
-            if(user.val()) {
+            if (user.val()) {
               storeData("user", user.val())
               navigation.replace("MainApp")
             }
           })
       })
       .catch(err => {
-        setLoading(false)
-        console.log({ err })
-        showMessage({
-          message: err.message,
-          type: "default",
-          backgroundColor: colors.error,
-          color: colors.white
-        })
+        loading(false)
+        showError(err.message)
       })
   }
 
+
+
   return (
-    <>
-      <View style={styles.page}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <ILLogo />
-          <Text style={styles.title}>Masuk dan mulai berkonsultasi</Text>
-          <Input
-            label="Email Address"
-            value={form.email}
-            onChangeText={(value) => setForm("email", value)}
-          />
-          <Gap height={24} />
-          <Input
-            label="Password"
-            value={form.password}
-            onChangeText={(value) => setForm("password", value)}
-            secureTextEntry
-          />
-          <Gap height={10} />
-          <Link title="Forgot My Password" size={12} />
-          <Gap height={40} />
-          <Button title="Sign In" onPress={login} />
-          <Gap height={30} />
-          <Link
-            onPress={() => navigation.navigate("Register")}
-            title="Create New Account"
-            size={16}
-            align="center" />
-        </ScrollView>
-      </View>
-      {
-        loading &&
-        <Loading />
-      }
-    </>
+    <View style={styles.page}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <ILLogo />
+        <Text style={styles.title}>Masuk dan mulai berkonsultasi</Text>
+        <Input
+          label="Email Address"
+          value={form.email}
+          onChangeText={(value) => setForm("email", value)}
+        />
+        <Gap height={24} />
+        <Input
+          label="Password"
+          value={form.password}
+          onChangeText={(value) => setForm("password", value)}
+          secureTextEntry
+        />
+        <Gap height={10} />
+        <Link title="Forgot My Password" size={12} />
+        <Gap height={40} />
+        <Button title="Sign In" onPress={login} />
+        <Gap height={30} />
+        <Link
+          onPress={() => navigation.navigate("Register")}
+          title="Create New Account"
+          size={16}
+          align="center" />
+      </ScrollView>
+    </View>
+
   )
 }
 

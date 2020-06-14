95b04d2f6d52243c7537500c80587b235c01ed06
ajengrf <ajengrf.12@gmail.com>
2020-06-14 17:51:07 +0700

    send chat data to firebase

--- src/pages/Chatting/index.js
+++ src/pages/Chatting/index.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import { Header, ChatItem, InputChat } from '../../components'
-import { fonts, colors } from '../../utils'
+import { fonts, colors, getData, showError } from '../../utils'
+import { Firebase } from '../../config'
 
 const Chatting = ({ navigation, route }) => {
   const dataDoctor = route.params
+  const [chatContent, setChatContent] = useState("")
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    getData("user")
+      .then(res => {
+        setUser(res)
+      })
+  }, [])
+
+  const chatSend = () => {
+    const today = new Date()
+    const hour = today.getHours()
+    const minutes = today.getMinutes()
+    const year = today.getFullYear()
+    const month = today.getMonth() + 1
+    const date = today.getDate()
+
+    const data = {
+      sendBy: user.uid,
+      chatDate: new Date().getTime(),
+      chatTime: `${hour}:${minutes} ${hour > 12 ? "PM" : "AM"}`,
+      chatContent: chatContent
+    }
+
+    Firebase.database()
+      .ref(`chatting/${user.uid}_${dataDoctor.uid}/allChat/${year}-${month}-${date}`)
+      .push(data)
+      .then(res => {
+        console.log({res})
+      })
+      .catch(err => {
+        showError(err.message)
+      })
+
+    setChatContent("")
+  }
+
 
   return (
     <View style={styles.page}>
@@ -25,9 +64,9 @@ const Chatting = ({ navigation, route }) => {
         </ScrollView>
       </View>
       <InputChat
-        value={""}
-        onChangeText={() => alert("change tesxt")}
-        onButtonPress={() => alert("button pressed")}
+        value={chatContent}
+        onChangeText={(value) => setChatContent(value)}
+        onButtonPress={chatSend}
       />
     </View>
   )
